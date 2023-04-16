f7f2704a09474ee3ad4432cd48df155932679ab4
Nina <preciouscornier13@gmail.com>
2023-04-16 14:38:59 -0400

    update

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-var audio = new Audio("sounds/crash.mp3", "sounds/kick-bass.mp3", "sounds/snare.mp3", );
-
-for (var i = 0; i<numberOfDrumButtons; i++) {
+for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function() {
-        console.log(this);
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
     });
 }
-document.querySelector(".w").addEventListener("click", function() {
-    audio.play("sounds/crash.mp3");
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
 });
-document.querySelector(".a").addEventListener("click", function() {
-    audio.play("sounds/kick-bass.mp3");
-});
-document.querySelector(".s").addEventListener("click", function() {
-    audio.play("sounds/snare.mp3");
-});
-document.querySelector(".d").addEventListener("click", function() {
-    audio.play("sounds/tom-1.mp3");
-});
-document.querySelector(".j").addEventListener("click", function() {
-    audio.play("sounds/tom-2.mp3");
-});
-document.querySelector(".k").addEventListener("click", function() {
-    audio.play("sounds/tom-3.mp3");
-});
-document.querySelector(".l").addEventListener("click", function() {
-    audio.play("sounds/tom-4.mp3");
-});
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+        case "a":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+        break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+        break;
+    }
+}
