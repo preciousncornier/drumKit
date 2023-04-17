aecbaf9b2a8b2575fffb7cf7e26d532a1cb65955
Nina <preciouscornier13@gmail.com>
2023-04-17 13:54:08 -0400

    update

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function() {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 document.addEventListener("keypress", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 function makeSound(key) {
     switch (key) {
@@ -38,5 +40,14 @@ function makeSound(key) {
             var tom4 = new Audio("sounds/tom-4.mp3");
                 tom4.play();
         break;
+        default: console.log(buttonInnerHTML);
     }
+    
+}
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 200);
 }
